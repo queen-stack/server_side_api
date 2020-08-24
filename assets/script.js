a06b354c2b6ec246d18f56f45576a5ddaabc6080
JQueen <queen_jenf@yahoo.com>
2020-08-23 18:31:05 -0700

    build file

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,13 @@
-var getUserRepos = function(user) {
-    // format the github api url
-    // the API for the weather app: 
-    //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={YOUR API KEY}
-    var apiUrl = "https://api.github.com/users/" + user + "/repos";
-  
-    // make a request to the url
-    fetch(apiUrl).then(function(response) {
-      response.json().then(function(data) {
-        console.log(data);
-      });
-    });
-  };
+var button = document.querySelector('.button')
+var inputValue = document.querySelector('.inputValue')
+var name = document.querySelector('.name');
+var description = document.querySelector('.description');
+var temperature = document.querySelector('.temperature');
 
-  /*requirements
- search for a city in the US
-Presented with today & future conditions for that city and that city is added to the search history
-local storage
-view current weather conditions for that city
-presented with city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-WHEN I view the UV index
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-WHEN I click on a city in the search history
-THEN I am presented with current and future conditions for that city
-
-Actions - 
-know that the images will be derived from the API
+button.addEventListener('click'.function(){
+  fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=588cfb5bf5bb11ec2e52fa3f89a7adc3')
+  .then(response => response.json())
+  then(data => console.log(data))
+  .catch(err => alert('Incorrect city name!'))
 
 
-  
